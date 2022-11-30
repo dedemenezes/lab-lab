f5f4c7d5c8db0cf0c28402feb97eee1a2002307f
Andre Menezes <djdedemenezes@gmail.com>
2022-11-30 20:10:01 -0300

    feat: add answer model

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,23 @@ User.create!(email: 'dede@lw.com', password: '123456')
 puts 'creating questions...'
 q1 = Question.create!(content: 'violencia e defesa do cidadao')
 q2 = Question.create!(content: 'Combate a probreza')
-# Answer.create!(
-#   content: 'o cidadao deve ter o direito ao porte de arma para garantir sua seguranca e de sua familia',
-#   question: q1
-# )
-# Answer.create!(
-#   content: 'o direito ao porte deve ser limitado pois aumeta a violencia',
-#   question: q1
-#   )
-# Answer.create!(
-#   content: 'programas sociais estimulam a ociosidade',
-#   question: q2
-# )
-# Answer.create!(
-#   content: 'programas sociais diminuem a desigualdade',
-#   question: q2
-# )
+
+puts 'creating answers...'
+Answer.create!(
+  content: 'o cidadao deve ter o direito ao porte de arma para garantir sua seguranca e de sua familia',
+  question: q1
+)
+Answer.create!(
+  content: 'o direito ao porte deve ser limitado pois aumeta a violencia',
+  question: q1
+  )
+Answer.create!(
+  content: 'programas sociais estimulam a ociosidade',
+  question: q2
+)
+Answer.create!(
+  content: 'programas sociais diminuem a desigualdade',
+  question: q2
+)
 
 puts 'finished zo/'
